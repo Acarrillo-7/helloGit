f46311aac34e18d8698aeab79e6b763782a14cd8
Acarrillo-07 <acarrillo7@huskers.unl.edu>
2024-08-30 18:40:39 -0500

    first commit

---
+++ src/main/java/com/college/Main.java
@@ -0,0 +1,8 @@
+package com.college;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("My name is Andrew Carrillo.");
+        System.out.println("I am a Computer Engineer Major");
+    }
+}
