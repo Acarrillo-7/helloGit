bbbc3e40346ce5ea404e53aa5c3c1f956a7618a2
Max Schessler <schessler.max@gmail.com>
2024-08-30 19:06:43 -0500

    added max

--- src/main/java/com/college/Main.java
+++ src/main/java/com/college/Main.java
@@ -3,6 +3,9 @@ package com.college;
 public class Main {
     public static void main(String[] args) {
         System.out.println("My name is Andrew Carrillo.");
-        System.out.println("I am a Computer Engineer Major");
+        System.out.println("I am a Computer Engineering Major");
+    
+        System.out.println("My name is Max Schessler");
+        System.out.println("I am a Computer Engineering Major");
     }
 }
